f709be096c9bfed45e1c61fcb8933cfa4692d9d3
losakovvitalik <losakovvitalik@gmail.com>
2025-03-15 22:51:01 +0300

    add tinder effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,64 @@
-'use client'; 
+'use client';
 import JobCard from '@/entities/job/ui/JobCard';
-import { motion } from 'motion/react';
+import { motion, useMotionValue, useTransform } from 'motion/react';
+import { Dispatch, SetStateAction, useState } from 'react';
+
+const jobs = ['тест 1', 'тест 2', 'тест 3', 'тест 4'];
 
 export default function Home() {
+  const [cards, setCards] = useState(jobs);
+
   return (
-    <div className="h-full p-4">
-      <motion.div
-        drag="x"
-        dragConstraints={{ left: 0, right: 0 }}
-        onDragEnd={(_, info) => {
-          if (info.offset.x > 100) {
-            console.log('swipe right')
-          };
-          if (info.offset.x < -100) {
-            console.log('swipe left')
-          };
-        }}
-      >
-        <JobCard />
-      </motion.div>
+    <div className="grid h-full place-items-center p-4 hover:cursor-grab active:cursor-grabbing">
+      {cards.map((job) => (
+        <Card job={job} key={job} cards={cards} setCards={setCards} />
+      ))}
     </div>
   );
 }
+
+const Card = ({
+  job,
+  cards,
+  setCards,
+}: {
+  job: string;
+  cards: string[];
+  setCards: Dispatch<SetStateAction<string[]>>;
+}) => {
+  const x = useMotionValue(0);
+  const opacity = useTransform(x, [-150, 0, 150], [0, 1, 0]);
+  const rotate = useTransform(x, [-150, 150], [-15, 15]);
+
+  const isFront = cards[cards.length - 1] === job;
+
+  const handleDragEnd = () => {
+    if (Math.abs(x.get()) > 50) {
+      setCards((pv) => pv.filter((v) => v !== job));
+    }
+  };
+
+  return (
+    <motion.div
+      drag="x"
+      className="origin-bottom"
+      key={job}
+      onDragEnd={handleDragEnd}
+      style={{
+        gridColumn: 1,
+        gridRow: 1,
+        opacity,
+        rotate,
+        x,
+        z: isFront ? 1 : 0,
+      }}
+      dragConstraints={{
+        left: 0,
+        right: 0,
+      }}
+      animate={{ scale: isFront ? 1 : 0.9 }}
+    >
+      <JobCard job={{ title: job }} />
+    </motion.div>
+  );
+};
--- src/entities/job/ui/JobCard.tsx
+++ src/entities/job/ui/JobCard.tsx
@@ -1,10 +1,18 @@
 import { Badge } from '@/components/ui/badge';
 import Typography from '@/components/ui/typography';
+import { cn } from '@/lib/utils';
 
-const JobCard = () => {
+interface JobCardProps {
+  job: {
+    title: string;
+  };
+  className?: string;
+}
+
+const JobCard = ({ job, className }: JobCardProps) => {
   return (
-    <div className="bg-secondary flex h-full w-full flex-col gap-2 rounded-xl p-4">
-      <Typography>Стажер / Frontend Специалист</Typography>
+    <div className={cn("bg-secondary flex h-full w-full flex-col gap-2 rounded-xl p-4", className)}>
+      <Typography>{job.title}</Typography>
       <Typography>Зарплата: 40 000 ₽</Typography>
       <Typography>Опыт работы: любой</Typography>
       <Typography>Компания: ООО TestCompany</Typography>
@@ -16,7 +24,7 @@ const JobCard = () => {
         eius cupiditate pariatur recusandae. Optio fuga magnam maiores reiciendis delectus facilis
         consectetur laboriosam vero consequatur.
       </Typography>
-      <div className="flex flex-wrap gap-1 mt-2">
+      <div className="mt-2 flex flex-wrap gap-1">
         <Badge>JavaScript</Badge>
         <Badge>TypeScript</Badge>
         <Badge>React</Badge>
