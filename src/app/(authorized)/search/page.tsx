7ff30a06cf18f6aa210a861d89665a0d2c5ea8a6
losakovvitalik <losakovgithub@gmail.com>
2025-04-27 20:39:30 +0300

    remove carousel

--- src/app/(authorized)/search/page.tsx
+++ src/app/(authorized)/search/page.tsx
@@ -19,12 +19,6 @@ const SearchPage = () => {
         </Typography>
         <VacancyCarousel className="mt-2" />
       </div>
-      <div className="mt-6">
-        <Typography className="font-bold" size={'xl'}>
-          Рекомендуемые компании
-        </Typography>
-        <CompanyCarousel className="mt-2" />
-      </div>
     </div>
   );
 };
--- src/entities/comment/ui/comment-card.tsx
+++ src/entities/comment/ui/comment-card.tsx
@@ -9,12 +9,16 @@ export interface CommentCardProps {
 const CommentCard = ({ comment }: CommentCardProps) => {
   return (
     <div className="flex gap-2">
-      <Image className="h-6 w-6" src={comment.user.photo?.url || '/avatar-placeholder.png'} alt="" width={24} height={24} />
+      <Image
+        className="h-6 w-6"
+        src={comment.user.photo?.url || '/avatar-placeholder.png'}
+        alt=""
+        width={24}
+        height={24}
+      />
       <div>
         <Typography size={'xs'}>{comment.user.name}</Typography>
-        <Typography size={'sm'}>
-          {comment.text}
-        </Typography>
+        <Typography size={'sm'}>{comment.text}</Typography>
       </div>
     </div>
   );
